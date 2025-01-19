9673e560968f52bc95120a2d2ba259d95bd1c904
sayan | NotDev <85256833+sayan20004@users.noreply.github.com>
2025-01-19 23:15:40 +0530

    Delete Hospital Project directory

--- Hospital Project/assets/js/index.js
+++
@@ -1,9 +0,0 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
-    smooth: true,
-});
-
-
-document.getElementById('book').addEventListener('click', function() {
-    document.getElementById('appointmentForm').classList.toggle('show');
-});
